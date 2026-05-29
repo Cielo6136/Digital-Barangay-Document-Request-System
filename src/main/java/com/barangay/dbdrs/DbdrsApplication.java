728e774419a051410d22a91c768a167906a245c1
Cielo6136 <gallanomarkcielo@gmail.com>
2026-05-29 18:47:12 +0800

    spring initialization

---
+++ src/main/java/com/barangay/dbdrs/DbdrsApplication.java
@@ -0,0 +1,13 @@
+package com.barangay.dbdrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbdrsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbdrsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/barangay/dbdrs/DbdrsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barangay.dbdrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbdrsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
